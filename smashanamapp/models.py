b543ccaad1a9a4e3458499e615eb950973d99d56
Amal C P <acp070146@gmail.com>
2023-03-11 12:09:26 +0530

    added user login and signup

--- smashanamapp/models.py
+++ smashanamapp/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+# from django.contrib.auth.models import User
+from datetime import datetime,date
+
 
 # Create your models here.
+class User(models.Model):
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    password = models.CharField(max_length=20)
+
+class Admin(models.Model):
+    aid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    password = models.CharField(max_length=20)
--- smashanamapp/urls.py
+++ smashanamapp/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
-from . import views
+from .views import signup,admin_home,login_admin,login,user_logout,admin_login,admin_logout,user_home
+from django import contrib
+from django.urls import include,path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',signup, name='signup'),
+    path('login/',login, name='login'),
+    path('login_admin/',login_admin, name='login_admin'),
+    path('admin_home/',admin_home, name='admin_home'),
+    path('admin_logout/',admin_logout, name='admin_logout'),
+    path('user_logout/',user_logout, name='user_logout'),
+    path('user_home/',user_home, name='user_home'),
+
 ]
